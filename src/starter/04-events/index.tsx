fc602575e7aad16bc0a130f4242106e5affed123
Radwan Mansur <raydiaz1899@gmail.com>
2025-10-18 15:36:53 +0300

    form data new knowledge

--- src/starter/04-events/index.tsx
+++ src/starter/04-events/index.tsx
@@ -10,16 +10,30 @@ function Component() {
     console.log(e.target.value);
     setEmail(e.target.value);
   }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    // const formData = new FormData(e.target as HTMLFormElement)
+    const data = Object.fromEntries(formData)
+    console.log(data);
+  }
   return (
     <div>
       <h2>React & Typescript</h2>
-      <form className="form">
-        <input type="text" placeholder="text" className="form-input mb-1" value={text} onChange={(e) =>
-          setText(e.target.value)
-        } />
+      <form className="form" onSubmit={handleSubmit}>
+        <input type="text" placeholder="text" className="form-input mb-1" value={text}
+          name='text'
+          onChange={(e) =>
+            setText(e.target.value)
+          } />
 
-        <input type="email" placeholder="email" className="form-input mb-1" value={email} onChange={handleChange
-        } />
+        <input type="email" placeholder="email"
+          name='email'
+          className="form-input mb-1" value={email} onChange={handleChange
+          } />
+
+        <button className="btn" type="submit">submit</button>
 
       </form>
     </div>
