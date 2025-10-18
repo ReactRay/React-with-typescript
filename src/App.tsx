385c1dfdc993a3a97b18ada9f59ce9e5da403fc2
Radwan Mansur <raydiaz1899@gmail.com>
2025-10-18 14:54:54 +0300

    types

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Component from './starter/01-return/index';
+import Component from './starter/02-props/index.tsx';
 
 
 function App() {
   return (
     <main>
-      <Component />
+      <Component name='john' id={1} />
     </main>
   );
 }
--- src/starter/02-props/index.tsx
+++ src/starter/02-props/index.tsx
@@ -1,8 +1,17 @@
-function Component() {
+
+
+type ComponentProps = {
+  name: string;
+  id: number;
+}
+
+function Component(props: ComponentProps) {
+
+
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>Props</h2>
+      <h2>Name: {props.name}</h2>
+      <h2>ID: {props.id}</h2>
     </div>
   );
 }
