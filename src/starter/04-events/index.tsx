3081d46effa57efebeff6ec12fb9f734843e68d6
Radwan Mansur <raydiaz1899@gmail.com>
2025-10-18 15:29:20 +0300

    event in ts

--- src/starter/04-events/index.tsx
+++ src/starter/04-events/index.tsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
+
 function Component() {
+
+
+  const [text, setText] = useState('');
+  const [email, setEmail] = useState('')
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    setEmail(e.target.value);
+  }
   return (
     <div>
       <h2>React & Typescript</h2>
-      <h2>Events</h2>
+      <form className="form">
+        <input type="text" placeholder="text" className="form-input mb-1" value={text} onChange={(e) =>
+          setText(e.target.value)
+        } />
+
+        <input type="email" placeholder="email" className="form-input mb-1" value={email} onChange={handleChange
+        } />
+
+      </form>
     </div>
   );
 }
