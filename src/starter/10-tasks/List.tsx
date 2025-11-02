9b80cb2f9878e347e5eb896878e161599bd5a777
Radwan Mansur <raydiaz1899@gmail.com>
2025-11-03 01:28:47 +0200

    finish todos with ts and local storage

--- src/starter/10-tasks/List.tsx
+++ src/starter/10-tasks/List.tsx
@@ -1,9 +1,24 @@
 
-function List() {
+import { Task } from "./types"
+type ListProps = {
+
+    toggleTask: (id: string) => void,
+    tasks: Task[]
+
+
+}
+
+
+function List({ toggleTask, tasks }: ListProps) {
     return (
-        <div>
-            List tsx
-        </div>
+        <ul className="list">
+            {tasks.map((task) => {
+                return <li key={task.id}>
+                    <p className="task-text">{task.description}</p>
+                    <input type="checkbox" checked={task.isCompleted} onChange={() => toggleTask(task.id)} />
+                </li>
+            })}
+        </ul>
     )
 }
 
--- src/starter/10-tasks/index.tsx
+++ src/starter/10-tasks/index.tsx
@@ -3,16 +3,39 @@ import { useState, useEffect } from "react";
 import List from "./List";
 import Form from "./Form";
 import { Task } from "./types";
+
+const loadTasks = (): Task[] => {
+  const tasks = localStorage.getItem('tasks');
+  if (tasks) {
+    return JSON.parse(tasks);
+  }
+  return []
+}
+
+const updateStorage = (tasks: Task[]): void => {
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+}
 function Component() {
 
-  const [tasks, setTasks] = useState<Task[]>([])
+  const [tasks, setTasks] = useState<Task[]>(loadTasks)
 
+  useEffect(() => {
+
+    updateStorage(tasks)
+  }, [tasks])
 
   const addTask = (task: Task): void => {
     setTasks([...tasks, task]);
+
   }
 
-  const toggleTask = () => {
+  const toggleTask = (id: string): void => {
+
+    const updatedTasks = tasks.map((task: Task) => {
+      return task.id === id ? { ...task, isCompleted: !task.isCompleted } : task
+    })
+    setTasks(updatedTasks)
+
 
   }
 
@@ -22,7 +45,7 @@ function Component() {
       <h2>React & Typescript</h2>
       <h2>Task List</h2>
       <Form addTask={addTask} />
-      <List />
+      <List tasks={tasks} toggleTask={toggleTask} />
     </div>
   );
 }
